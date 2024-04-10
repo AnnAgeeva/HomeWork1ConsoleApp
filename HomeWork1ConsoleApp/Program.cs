dd937d33110e7d57c767daffbad94526dbe419cc
AnnAgeeva <anka-287@yandex.ru>
2024-04-10 22:15:16 +0300

    Добавьте файлы проекта.

---
+++ HomeWork1ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, Мир!");    //вывод текста на экран
+            Console.ReadKey();                 //задержка черного экрана
+        }
+    }
+}
